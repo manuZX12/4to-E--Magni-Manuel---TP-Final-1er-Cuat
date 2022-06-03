427188ebff91d51cec7ac996e0c88f7a1cc6faf2
Manuel Magni <61920152+manuZX12@users.noreply.github.com>
2022-06-03 14:16:01 -0300

    Level 4 Mechanics Complete

---
+++ Assets/Fourth Game/KeyBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyBehaviour : MonoBehaviour
+{
+    public GameObject Key1;
+    public GameObject Door;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision Col)
+    {
+        if(Col.gameObject.name == "Key")
+        {
+            Destroy(Key1);
+            Destroy (Door);
+        }
+    }
+}
