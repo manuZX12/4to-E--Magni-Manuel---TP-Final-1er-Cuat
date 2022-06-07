aaf14e79cb2de0073d0a5a40a5d5c0aa50d167b9
Manuel Magni <61920152+manuZX12@users.noreply.github.com>
2022-06-06 21:13:06 -0300

    Completed various functions
    Level 2,3,4  Design
    NexLevel Behaviour

---
+++ Assets/Fourth Game/CombinatedKeys.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombinatedKeys : MonoBehaviour
+{
+    public GameObject BigBox;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "PickedObject")
+        {
+            if (transform.position.y > 0.3f)
+            {
+                Instantiate(BigBox);
+                BigBox.transform.position = transform.position;
+                Destroy(gameObject);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+            
+            
+        }
+    }
+}
---
+++ Assets/Third Game/NextLevel.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextLevel : MonoBehaviour
+{
+    public GameObject hand;
+    private GameObject PickedObject;
+    // Update is called once per frame
+    void Update()
+    {
+        if (PickedObject != null)
+        {
+            if (Input.GetMouseButtonUp(1))
+            {
+                PickedObject.GetComponent<Rigidbody>().useGravity = true;
+                PickedObject.GetComponent<Rigidbody>().isKinematic = false;
+                //hago al objeto hijo de nada
+                PickedObject.gameObject.transform.SetParent(null);
+                PickedObject = null;
+            }
+        }
+    }
+    private void OnTriggerStay(Collider col)
+    {
+        if (col.gameObject.CompareTag("FN"))
+        {
+            if (Input.GetMouseButtonDown(1) && PickedObject == null)
+            {
+                
+
+                SceneManager.LoadScene("Room4");
+
+
+
+            }
+        }
+    }
+    
+}
+
